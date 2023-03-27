3a347980aeab3b98ce51ec6c0413747971a7d99f
kiran <example.1>
2023-03-27 08:59:20 +0545

    Add project files.

---
+++ day2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Bitwiseoperator();
+           logicaloperater();
+            Console.ReadKey();
+        }
+        static void logicaloperater()//example of logical operator
+        {
+            Console.WriteLine("enter your college name");
+            string collegeName=Console.ReadLine();
+            Console.WriteLine("enter your program");
+            string programName=Console.ReadLine();
+            if (string.IsNullOrEmpty(collegeName))
+            {
+                Console.WriteLine("please enter the college name");
+            }
+            else if (collegeName.ToUpper()=="BMC"&& programName.ToUpper()=="CSIT")
+            {
+                Console.WriteLine("You are welcome in lab");
+            }
+            else
+            {
+                Console.WriteLine("you maynot enter");
+
+            }
+        }
+
+        static void Bitwiseoperator() //example of bitwise operator
+        {
+
+        }
+
+        
+    }
+}
