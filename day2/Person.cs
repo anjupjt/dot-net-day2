0b41d3dce6abb8ae072aa9adb558e717ec2c3371
kiran <example.1>
2023-03-28 09:16:42 +0545

    day3drdrdr

---
+++ day2/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day21
+{
+    internal class Person
+    {
+        int numberofEyes;
+       public int noofEyes { get { return numberofEyes; } set { numberofEyes = value; } }
+        public string haircolor { get; set; }
+        public void EatingHabit() //take
+        {
+            Console.WriteLine("I usually have momos in lunch");
+        }
+        public void EatingHabit( string myLunch)//this function only give
+        {
+            Console.WriteLine("I usually have "+myLunch+" in lunch");
+        }
+        public string EatinngHabit(string myLunch)//return and take
+        {
+           return("I usually have "+myLunch+" in lunch");
+        }
+
+    }
+
+}
--- day2/Program.cs
+++ day2/Program.cs
@@ -3,42 +3,54 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
-namespace day2
+using day21;
+namespace day21
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Bitwiseoperator();
-           logicaloperater();
+           // Bitwiseoperator();
+           //logicaloperater();
+           Person anju=new Person();
+           // anju.numberofEyes = 2;
+            anju.noofEyes = 2;
+            anju.haircolor = "black";
+            anju.EatingHabit();
+            anju.EatingHabit("chowmein");
+            string myLunch = anju.EatinngHabit("Biryani");
+            Console.WriteLine(myLunch); 
+            Console.WriteLine("you have " + anju.noofEyes + " eyes");
+            Console.WriteLine("your hair color is "+anju.haircolor);
+
             Console.ReadKey();
+       
         }
-        static void logicaloperater()//example of logical operator
-        {
-            Console.WriteLine("enter your college name");
-            string collegeName=Console.ReadLine();
-            Console.WriteLine("enter your program");
-            string programName=Console.ReadLine();
-            if (string.IsNullOrEmpty(collegeName))
-            {
-                Console.WriteLine("please enter the college name");
-            }
-            else if (collegeName.ToUpper()=="BMC"&& programName.ToUpper()=="CSIT")
-            {
-                Console.WriteLine("You are welcome in lab");
-            }
-            else
-            {
-                Console.WriteLine("you maynot enter");
+        //static void logicaloperater()//example of logical operator
+        //{
+        //    Console.WriteLine("enter your college name");
+        //    string collegeName=Console.ReadLine();
+        //    Console.WriteLine("enter your program");
+        //    string programName=Console.ReadLine();
+        //    if (string.IsNullOrEmpty(collegeName))
+        //    {
+        //        Console.WriteLine("please enter the college name");
+        //    }
+        //    else if (collegeName.ToUpper()=="BMC"&& programName.ToUpper()=="CSIT")
+        //    {
+        //        Console.WriteLine("You are welcome in lab");
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("you maynot enter");
 
-            }
-        }
+        //    }
+        //}
 
-        static void Bitwiseoperator() //example of bitwise operator
-        {
+        //static void Bitwiseoperator() //example of bitwise operator
+        //{
 
-        }
+        //}
 
         
     }
